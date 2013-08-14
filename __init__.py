2e89da189e8d6a54cac2cab63940ccfc7a44a288
Jason Cheatham <j.cheatham@gmail.com>
2013-08-14 19:56:12 -0400

    Update version

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 '''Utility classes and functions for creating Alfred workflows'''
 
-__version__ = '20130814.0'
+__version__ = '20130814.1'
 
 from .alfred import (AlfredWorkflow, Item, LINE)
 from .keychain import Keychain
