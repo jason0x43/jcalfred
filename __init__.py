765499d1504aade50d1a735b7e3b29f1d7455750
Jason Cheatham <j.cheatham@gmail.com>
2013-08-14 16:49:48 -0400

    Add module version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+__version__ = '20130814.0'
+
 from .alfred import (AlfredWorkflow, Item, get_from_user, get_confirmation,
                      fuzzy_match_list, LINE, run_script, show_message,
                      BUNDLE_INFO)
