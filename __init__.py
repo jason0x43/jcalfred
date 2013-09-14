1ba0a6f93683a0e8f020abe5f34624db33c3a470
Jason Cheatham <j.cheatham@gmail.com>
2013-09-13 23:28:22 -0400

    Update the package version

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 '''Utility classes and functions for creating Alfred workflows'''
 
-__version__ = '20130909.0'
+__version__ = '20130913.0'
 
 from .alfred import Workflow, WorkflowInfo, Item, JsonFile, LINE
 from .keychain import Keychain
