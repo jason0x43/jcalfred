fb3177c2b6987eb6ea8fc945bc049ae99ff1d23e
Jason Cheatham <j.cheatham@gmail.com>
2013-12-14 22:48:49 -0500

    Change to semantic versioning

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 '''Utility classes and functions for creating Alfred workflows'''
 
-__version__ = '20130915.0'
+__version__ = '0.1.1'
 
 from .alfred import Workflow, WorkflowInfo, Item
 from .jsonfile import JsonFile
